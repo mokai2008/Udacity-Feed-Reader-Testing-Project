abac9d41a7f18001198f1b96da0319fa7c7e750c
Michael Gad <mokai2008@gmail.com>
2018-08-01 21:42:16 +0300

    did the last test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -94,6 +94,10 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    
+    describe('Initial Entries', function() {
+        
+    
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -101,11 +105,40 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        
+        beforeEach(function(done){
+           loadFeed(0, function(){
+               done();
+           }); 
+        });
+        
+        it('at least has one entry', function(done) {
+            var entries = $('.feed .entry').length;
+           expect(entries).toBeGreaterThan(0);
+            done();
+        });
+        
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    
+    describe('New Feed Selection', function(){
+        
+    
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done){
+            loadFeed(0, function(){
+               var newFeed = $('feed').html();
+                loadFeed(1, done);
+            });
+        });
+        
+        it('loaded with the new feed', function(){
+           expect($('.feed').html()).not.toEqual(newFeed);
+        });
+    });
 }());
